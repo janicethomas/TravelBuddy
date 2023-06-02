698dc197224d8d0a9d3e6d9825f159401ac5650e
janicethomas <janice.thomas@outlook.com>
2023-06-03 00:16:48 +0530

    added route from login to notes view

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/login/': (context) => const LoginView(),
         '/register/': (context) => const RegisterView(),
+        '/notes/': (context) => const NotesView(),
       },
     );
   }
